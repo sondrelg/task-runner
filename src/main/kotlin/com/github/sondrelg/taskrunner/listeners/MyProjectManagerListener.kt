4058190b2233f760f35c6d243c2546856126edb2
GitHub Action <action@github.com>
2020-08-20 07:54:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/sondrelg/taskrunner/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/sondrelg/taskrunner/listeners/MyProjectManagerListener.kt
@@ -1,8 +1,8 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.sondrelg.taskrunner.listeners
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.sondrelg.taskrunner.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/sondrelg/taskrunner/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sondrelg/taskrunner/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sondrelg.taskrunner.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sondrelg.taskrunner.MyBundle
 
 class MyApplicationService {
 
